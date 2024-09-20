ff273fcf639834a337a55116a5f6e19fa5d3b671
jamor <jamor.devbr@gmail.com>
2024-09-20 20:06:28 -0300

    avaliacao - execicio 01

---
+++ JavaScript_Developer/08 - avaliacao/exercicio2.js
@@ -0,0 +1,20 @@
+/*
+2) Faça um programa que receba N (quantidade de números) e seus respectivos valores.
+
+Imprima o maior numero par e o menor numero impar.
+
+Exemplo:
+Entrada:
+    5
+    3
+    4
+    1
+    10
+    8
+
+Saída: 
+    Maior número para: 10
+    Menor número impar: 1         
+        
+*/
+
